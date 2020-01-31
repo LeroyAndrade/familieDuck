5b39c1099f126af131cb7087c2fee383d3356985
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-31 10:52:24 +0100

    Initial push
    Initial push

--- neefjes.js
+++ neefjes.js
@@ -1,4 +1,5 @@
 // alle plaatjes van de duckfamilie in een array
+
 const plaatjes  = document.querySelectorAll('.duck img');
 const bolletjes = document.getElementById('bolletjes');
 
@@ -6,14 +7,30 @@ const bolletjes = document.getElementById('bolletjes');
 document.getElementsByClassName('info')[0].style.height = document.getElementsByClassName('duck')[0].clientHeight +'px';
 
 for(let i=0; i<plaatjes.length; i++) {
+  let box = document.getElementsByTagName('img')[i];
+
     plaatjes[i].addEventListener('mouseenter', vulKopAan);
     plaatjes[i].addEventListener('mouseleave', resetKop);
     // hier kun je nog 2 events aan toevoegen: click en dblclick
+    box.addEventListener('click', function(){
+//1:
+  let border=document.getElementById('one');
+
+      let text=document.getElementById('informatie');
+
+      text.innerHTML=this.dataset.info;
+      border.style.borderColor=this.dataset.kleur;
+      document.getElementById('textBox').innerHTML += "geslaagd";
+//this.dataset.clicks+1;
+        });
+//2:
 }
+
+
 
 function vulKopAan() {
     document.getElementById('duck').innerText = ": "+this.title;
-    document.getElementById('kop').style.color  = this.dataset.kleur;
+    document.getElementById('kop').style.color = this.dataset.kleur;
 }
 
 function resetKop() {
@@ -21,23 +38,28 @@ function resetKop() {
     document.getElementById('kop').style.color  = 'red';
 }
 
-/*  
+
+
+/*
     Maak een functie, die aangeroepen wordt door een klik op de afbeelding.
-    Deze moet om de geklikte afbeelding een rand maken met de kleur, die 
+    Deze moet om de geklikte afbeelding een rand maken met de kleur, die
     te vinden is in het data-kleur attribuut.
     Het element.dataset.eigenschap heb je van een element de waarde van 'data-eigenschap'
+*/
 
+
+/*
     Bovendien moet de sectie info gevuld worden: zowel de kop h3#titel als de p#info.
     De inhoud staat in de het alt-attribuut als een ander data-attribuut van de afbeelding.
-    
+
     */
 
 
-/** 
+/**
     Maak noeg een functie die aangeroepen wordt door een dubbelklik.
-    In de attributen van elke afbeelding staat een data-attribuut, data-clicks, 
+    In de attributen van elke afbeelding staat een data-attribuut, data-clicks,
     die moet worden opgehoogd.
     Bovendien moet de inhoud van die data-clicks worden ingevoegd in de p.likes.
-    Tot slot moet er een div met de class 'bolletje' en de achtergrondkleur uit 
+    Tot slot moet er een div met de class 'bolletje' en de achtergrondkleur uit
     data-kleur toegevoegd worden aan het element div#bolletjes
 */
